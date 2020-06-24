c268934f94068ad9564dc4ffcf0f8b9557be4656
AdaCaoShiyu <59095141+AdaCaoShiyu@users.noreply.github.com>
2020-06-24 11:56:21 +0100

    Update rmweather_LondonCity_NOx.R
    Make the code more clear

--- rmweather_LondonCity_NOx.R
+++ rmweather_LondonCity_NOx.R
@@ -1,5 +1,7 @@
 library(remotes)
 getwd()
+
+# Set the working directory
 setwd('C:\\Users\\csy\\OneDrive - Imperial College London\\MSc Tansport\\Dissertation\\Environment\\Coronavirus\\R\\Weather Normalisation\\06-14')
 
 # Install rmweather
@@ -23,13 +25,14 @@ data_london$date <- as.POSIXct(strptime(data_london$date,"%d/%m/%Y"))
 data_london$date_end <- as.POSIXct(strptime(data_london$date_end,"%d/%m/%Y %H:%M"))
 nox$date_end <- as.POSIXct(strptime(nox$date_end,"%d/%m/%Y %H:%M"))
 
+# Have a look at the structure, date variables should be in POSIXct, while meteorological variables should be in numirical type.
 str(data_london)
 str(nox)
 
-#Acquire Date_Julian
+# Acquire Date_Julian
 data_london$day_julian <- as.numeric(format(data_london$date, "%j"))
 
-#Save the dataset with Julian
+# Save the dataset with Julian
 write.csv(data_london, 'data_london0614.csv')
 
 #Join together air quality and meteorology
@@ -37,7 +40,7 @@ mydata <- left_join(data_london, nox, by = "date_end")
 write.csv(mydata,'mydata0614.csv')
 
 
-#data_london_prepared <- data_london 
+# data_london_prepared <- data_london 
 # Only use data with valid wind speeds, nox will become the dependent variable
 data_london_prepared <- mydata %>% 
   filter(!is.na(nox)) %>% 
@@ -50,7 +53,7 @@ set.seed(2)
 data_london_prepared$set = sample(2,nrow(data_london_prepared),replace = TRUE, prob = c(0.7,0.3))
 data_london_prepared$set <- as.character(data_london_prepared$set)
 
-# Replacement
+# Replacement number into "training" and "testing"
 for (i in 1:43847)
 { 
   if(data_london_prepared[i,1] == "1") {
@@ -95,12 +98,6 @@ list_normalised$model %>%
   rmw_plot_importance()
 
 
-### Prepare example data
-data_london_prepared <- data_london %>% 
-  filter(variable == "no2") %>% 
-  rmw_prepare_data()
-
-
 # Check if model has suffered from overfitting
 rmw_predict_the_test_set(
   model = list_normalised$model,
@@ -127,7 +124,7 @@ data_pd %>%
   filter(variable != "date_unix") %>% 
   rmw_plot_partial_dependencies()
 
-#Accuracy
+# Accuracy
 install.packages(caret)
 library(caret)
 p1 = predict(RFM,newdata = Training)
